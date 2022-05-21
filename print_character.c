1065892137fea9c521f56ef2470cdd111c3e1ba5
Iam-kauthar <abioyekaosarat5@gmail.com>
2022-05-21 05:11:32 -0700

    Updating Task 0

---
+++ print_character.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdarg.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+/**
+ * _print_char - is a function to print character
+ * @args: receives the arguments
+ * Return: 1 on success.
+ */
+
+int _print_char(va_list args)
+{
+	unsigned char c;
+
+	c = va_arg(args, int);
+	_putchar(c);
+	return (1);
+}
